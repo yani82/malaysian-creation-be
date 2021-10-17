5f76845ac32297e3f6cc14377beed0c5d4442f7c
yani82 <yani.iman1@gmail.com>
2021-10-17 02:31:00 -0400

    set up controllers

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,3 +1,33 @@
 class Api::V1::CategoriesController < ApplicationController
     
+    def index 
+        categories = category.all
+        render json: categories
+    end
+
+    def create
+        category = Category.new(category_params)
+        if category.save 
+            render json: category 
+        else 
+            render json: {error: 'Error creating category'}
+        end 
+    end
+
+    def show 
+        category = Category.find(params[:id])
+        render json: category 
+    end
+
+    def destroy
+        category = Category.find(params[:id])
+        category.destroy
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+    end 
+
 end
--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,3 +1,34 @@
 class Api::V1::ItemsController < ApplicationController
     
+    def index 
+        items = item.all
+        render json: items
+    end
+
+    def create
+        item = Item.new(item_params)
+        if item.save 
+            render json: item 
+        else 
+            render json: {error: 'Error creating item'}
+        end 
+    end
+
+    def show 
+        item = Item.find(params[:id])
+        render json: item 
+    end
+
+    def destroy
+        item = Item.find(params[:id])
+        item.destroy
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:category_id, :name, :description, :price)
+    end 
+    
+
 end
